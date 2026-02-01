14300a82353f207c188f6e370bfefe0fdcaadea8
FitDEWROS <labnap2@gmail.com>
2026-02-01 22:27:02 +0300

    Ignore Flutter build artifacts

--- app_android/lib/app.dart
+++ app_android/lib/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:app_links/app_links.dart';
-import 'config.dart';
 import 'services/auth_service.dart';
 import 'screens/login_screen.dart';
 import 'screens/programs_screen.dart';
@@ -18,36 +16,18 @@ class FitDewApp extends StatefulWidget {
 class _FitDewAppState extends State<FitDewApp> {
   final _auth = AuthService();
   final _navigatorKey = GlobalKey<NavigatorState>();
-  late final AppLinks _appLinks;
   String _initialRoute = '/login';
 
   @override
   void initState() {
     super.initState();
-    _appLinks = AppLinks();
     _bootstrap();
-    _appLinks.uriLinkStream.listen((uri) {
-      if (uri.scheme == AppConfig.authScheme) {
-        _auth.handleAuthUri(uri).then((token) {
-          if (token != null) {
-            _navigatorKey.currentState?.pushReplacementNamed('/programs');
-          }
-        });
-      }
-    });
   }
 
   Future<void> _bootstrap() async {
     final token = await _auth.getToken();
     if (token != null && token.isNotEmpty) {
       setState(() => _initialRoute = '/programs');
-    }
-    final initialUri = await _appLinks.getInitialLink();
-    if (initialUri != null && initialUri.scheme == AppConfig.authScheme) {
-      final handled = await _auth.handleAuthUri(initialUri);
-      if (handled != null) {
-        setState(() => _initialRoute = '/programs');
-      }
     }
   }
 
